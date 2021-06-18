19e99143002da16c87dc95ccb0460a17315ce5e8
James <67937330+FrontEndDev-James@users.noreply.github.com>
2021-06-18 00:48:32 -0500

    React Webull site

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import GlobalStyle from "./globalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
